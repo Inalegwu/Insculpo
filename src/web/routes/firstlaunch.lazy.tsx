763d6ca532c8afa5eceade4b4408977012f9f805
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-04-19 22:04:00 +0100

    fixes

--- src/web/routes/firstlaunch.lazy.tsx
+++ src/web/routes/firstlaunch.lazy.tsx
@@ -7,7 +7,13 @@ export const Route = createLazyFileRoute("/firstlaunch")({
 
 function FirstLaunch() {
   return (
-    <Flex width="100%" height="100%" align="center" justify="center">
+    <Flex
+      direction="column"
+      width="100%"
+      height="100%"
+      align="center"
+      justify="center"
+    >
       <Text size="9">Insculpo</Text>
       <Text size="7" className="text-gray-400">
         The simplest note-taking app there is
