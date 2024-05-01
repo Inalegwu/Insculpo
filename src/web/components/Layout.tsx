b85dcfa1b48a5c009bdc6f77de6af07ca5c63ae9
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-01 05:12:28 +0100

    different views for notes and notebooks

--- src/web/components/Layout.tsx
+++ src/web/components/Layout.tsx
@@ -1,5 +1,15 @@
-import { useObservable } from "@legendapp/state/react";
-import { Box, Button, Flex, IconButton, Text, Tooltip } from "@radix-ui/themes";
+import { Switch, useObservable } from "@legendapp/state/react";
+import {
+  Box,
+  Button,
+  ContextMenu,
+  Flex,
+  IconButton,
+  Popover,
+  Text,
+  TextField,
+  Tooltip,
+} from "@radix-ui/themes";
 import t from "@src/shared/config";
 import { Document, FlatList } from "@src/web/components";
 import { useDebounce, useEditor, useTimeout, useWindow } from "@src/web/hooks";
@@ -10,12 +20,15 @@ import type React from "react";
 import { useCallback, useEffect } from "react";
 import toast from "react-hot-toast";
 import {
+  FiArrowLeft,
+  FiArrowRight,
   FiDownload,
   FiInfo,
   FiMinus,
   FiPlus,
   FiSettings,
   FiSidebar,
+  FiTrash,
   FiX,
 } from "react-icons/fi";
 
@@ -23,19 +36,25 @@ type LayoutProps = {
   children?: React.ReactNode;
 };
 
+type Route = {
+  key: "Notes" | "Notebooks";
+};
+
 export default function Layout({ children }: LayoutProps) {
   const { mutate: minimize } = t.window.minimize.useMutation();
   const { mutate: close } = t.window.closeWindow.useMutation();
   const routeState = useRouterState();
   const nav = useNavigate();
+  const utils = t.useUtils();
 
   const [text, activeNoteId] = useEditor();
 
-  const { data: notes } = t.notes.getNotes.useQuery();
   const { data: ver } = t.version.useQuery();
 
   const passedThres = useObservable(false);
   const sideBarFocus = useObservable(false);
+
+  const route = useObservable<Route>({ key: "Notes" });
 
   const { mutate: dumpAllNotes } = t.notes.dumpNotes.useMutation({
     onSuccess: () => {
@@ -85,18 +104,6 @@ export default function Layout({ children }: LayoutProps) {
       });
     }
   }, [nav, routeState, activeNoteId, text]);
-
-  const handleSearchResultClicked = useCallback(
-    (id: string) => {
-      activeNoteId.set(id);
-      if (routeState.location.pathname !== "/") {
-        nav({
-          to: "/",
-        });
-      }
-    },
-    [nav, routeState, activeNoteId],
-  );
 
   return (
     <Flex
@@ -189,28 +196,37 @@ export default function Layout({ children }: LayoutProps) {
                     <FiX size={11} />
                   </Button>
                 </Flex>
-                <FlatList
-                  data={notes || []}
-                  className="px-2"
-                  listHeaderComponent={() => (
-                    <Flex
-                      align="center"
-                      direction="row"
-                      justify="end"
-                      className="py-1 px-0"
-                    >
-                      <Text
-                        className="text-[10px] text-gray-400 font-light"
-                        color="gray"
-                      >
-                        All Notes
-                      </Text>
-                    </Flex>
-                  )}
-                  renderItem={({ item, index }) => (
-                    <Document doc={item} key={index} />
-                  )}
-                />
+                <Flex
+                  align="center"
+                  justify="end"
+                  gap="4"
+                  className="items-center mb-4 px-3"
+                >
+                  <Button
+                    variant="ghost"
+                    size="1"
+                    color="gray"
+                    className="w-2 h-4 rounded-full cursor-pointer"
+                    onClick={() => route.set({ key: "Notes" })}
+                  >
+                    <FiArrowLeft />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    size="1"
+                    color="gray"
+                    className="w-2 h-4 rounded-full cursor-pointer"
+                    onClick={() => route.set({ key: "Notebooks" })}
+                  >
+                    <FiArrowRight />
+                  </Button>
+                </Flex>
+                <Switch value={route.get().key}>
+                  {{
+                    Notes: () => <NoteListView />,
+                    Notebooks: () => <NotebookListView />,
+                  }}
+                </Switch>
               </Flex>
               {/* bottom bar */}
               <Flex
@@ -281,24 +297,127 @@ export default function Layout({ children }: LayoutProps) {
   );
 }
 
-// not useful for now...
-//  <DropdownMenu.Root>
-//   <DropdownMenu.Trigger>
-//     <IconButton variant="ghost" size="2" radius="full">
-//       <Sliders />
-//     </IconButton>
-//   </DropdownMenu.Trigger>
-//   <DropdownMenu.Content
-//     defaultValue="dateCreated"
-//     size="1"
-//     className="dark:bg-slate-700"
-//   >
-//     <DropdownMenu.Label>Sort By</DropdownMenu.Label>
-//     <DropdownMenu.CheckboxItem textValue="dateCreated">
-//       <Text>Date Created</Text>
-//     </DropdownMenu.CheckboxItem>
-//     <DropdownMenu.CheckboxItem textValue="dateUpdated">
-//       <Text>Recently Updated</Text>
-//     </DropdownMenu.CheckboxItem>
-//   </DropdownMenu.Content>
-// </DropdownMenu.Root>;
+function NoteListView() {
+  const { data: notes } = t.notes.getNotes.useQuery();
+  return (
+    <FlatList
+      data={notes || []}
+      className="px-2"
+      listHeaderComponent={() => (
+        <Flex
+          align="center"
+          direction="row"
+          justify="end"
+          className="py-1 px-0"
+        >
+          <Text className="text-[10px] text-gray-400 font-light" color="gray">
+            All Notes
+          </Text>
+        </Flex>
+      )}
+      renderItem={({ item, index }) => <Document doc={item} key={index} />}
+    />
+  );
+}
+
+function NotebookListView() {
+  const utils = t.useUtils();
+  const { data: notebooks } = t.notebooks.getNotebooks.useQuery();
+
+  const notebookName = useObservable("");
+
+  const { mutate: createNotebook } = t.notebooks.createNotebook.useMutation({
+    onSuccess: () => {
+      utils.notebooks.getNotebooks.invalidate();
+    },
+  });
+
+  const { mutate: deleteNotebook } = t.notebooks.deleteNotebook.useMutation({
+    onSuccess: () => {
+      utils.notebooks.getNotebooks.invalidate();
+    },
+  });
+
+  const saveNotebook = useCallback(() => {
+    createNotebook({
+      name: notebookName.get(),
+    });
+  }, [createNotebook, notebookName]);
+
+  return (
+    <FlatList
+      data={notebooks || []}
+      className="px-2"
+      listHeaderComponent={() => (
+        <Flex
+          align="center"
+          direction="row"
+          justify="between"
+          className="py-1 px-0"
+        >
+          <Popover.Root>
+            <Popover.Trigger>
+              <Button
+                variant="ghost"
+                className="w-2 h-4 rounded-full ml-1"
+                color="gray"
+              >
+                <FiPlus />
+              </Button>
+            </Popover.Trigger>
+            <Popover.Content className="px-3 py-4">
+              <Flex direction="column" gap="2">
+                <Text size="1" className="text-gray-400">
+                  Notebook name
+                </Text>
+                <TextField.Root>
+                  <TextField.Input
+                    onChange={(e) => notebookName.set(e.currentTarget.value)}
+                  />
+                </TextField.Root>
+                <Popover.Close>
+                  <Button size="2" variant="soft" onClick={saveNotebook}>
+                    <Text size="1">Create</Text>
+                  </Button>
+                </Popover.Close>
+              </Flex>
+            </Popover.Content>
+          </Popover.Root>
+          <Text className="text-[10px] text-gray-400 font-light" color="gray">
+            All Notebooks
+          </Text>
+        </Flex>
+      )}
+      renderItem={({ item }) => (
+        <ContextMenu.Root>
+          <ContextMenu.Trigger className="cursor-pointer">
+            <Flex
+              direction="column"
+              align="end"
+              justify="center"
+              className="py-2"
+            >
+              <Text weight="bold" className="text-[11.5px]" color="iris">
+                {item.name}
+              </Text>
+              <Text className="text-[10px] text-gray-400">
+                {item.notes.length} Notes
+              </Text>
+            </Flex>
+          </ContextMenu.Trigger>
+          <ContextMenu.Content size="1" variant="soft">
+            <ContextMenu.Item
+              color="red"
+              onClick={() => deleteNotebook({ bookId: item.id })}
+            >
+              <Flex gap="1" align="center">
+                <FiTrash />
+                <Text size="1">Delete Notebook</Text>
+              </Flex>
+            </ContextMenu.Item>
+          </ContextMenu.Content>
+        </ContextMenu.Root>
+      )}
+    />
+  );
+}
